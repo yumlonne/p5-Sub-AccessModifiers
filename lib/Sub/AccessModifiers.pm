da58750c0f3c050fdcc7e58f698928d6d2b7ffda
yumlonne <yumlonne@gmail.com>
2018-11-03 00:15:03 +0900

    implement functions

--- lib/Sub/AccessModifiers.pm
+++ lib/Sub/AccessModifiers.pm
@@ -3,9 +3,26 @@ use 5.008001;
 use strict;
 use warnings;
 
+use Exporter qw(import);
+our @EXPORT_OK = qw(public protected private);
+
 our $VERSION = "0.01";
 
+sub public {}
 
+sub protected {
+    my ($c0_pkg, $c0_filename, $c0_line, $c0_sub) = caller(0);
+    my ($c1_pkg, $c1_filename, $c1_line)          = caller(1);
+    die "$c0_sub is Private at $c1_filename line $c1_line.\n"
+        if $c1_pkg->isa($c0_pkg);
+}
+
+sub private {
+    my ($c0_pkg, $c0_filename, $c0_line, $c0_sub) = caller(0);
+    my ($c1_pkg, $c1_filename, $c1_line)          = caller(1);
+    die "$c0_sub is Private at $c1_filename line $c1_line.\n"
+        if $c1_pkg eq $c0_pkg;
+}
 
 1;
 __END__

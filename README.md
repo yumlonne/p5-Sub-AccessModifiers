b91d24d0945104f5da6402a0eb1cd49a5c574fa9
yumlonne <yumlonne@gmail.com>
2018-11-02 23:57:39 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Sub::AccessModifiers - It's new $module
+
+# SYNOPSIS
+
+    use Sub::AccessModifiers;
+
+# DESCRIPTION
+
+Sub::AccessModifiers is ...
+
+# LICENSE
+
+Copyright (C) yumlonne.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+yumlonne <yumlonne@gmail.com>
---
+++ lib/Sub/AccessModifiers.pm
@@ -0,0 +1,39 @@
+package Sub::AccessModifiers;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sub::AccessModifiers - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sub::AccessModifiers;
+
+=head1 DESCRIPTION
+
+Sub::AccessModifiers is ...
+
+=head1 LICENSE
+
+Copyright (C) yumlonne.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+yumlonne E<lt>yumlonne@gmail.comE<gt>
+
+=cut
+
